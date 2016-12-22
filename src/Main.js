89926ec7fd5da988667143aecc0626ae61ad9b84
Juho Sepänmaa <sepanmaa@cs.helsinki.fi>
2016-12-22 02:48:59 +0200

    refactor foreign functions

--- src/Main.js
+++ src/Main.js
@@ -3,19 +3,23 @@
 
 exports.setImageData = function(imgData) {
     return function(array) {
-	var data = imgData.data;
-	for (var i = 0; i < array.length; i++)
-	    data[i] = array[i]
-	return function (){return imgData};
+	return function() {
+	    var data = imgData.data;
+	    for (var i = 0; i < array.length; i++)
+		data[i] = array[i]
+	    return imgData;
+	}
     }
 }
 
 exports.canvasBoundingRect = function(canvas) {
-    var rect = canvas.getBoundingClientRect();
-    var o = {};
-    o.x = rect.left;
-    o.y = rect.top;
-    o.w = rect.right-rect.left;
-    o.h = rect.bottom-rect.top;
-    return function(){return o};
+    return function() {
+	var rect = canvas.getBoundingClientRect();
+	var o = {};
+	o.x = rect.left;
+	o.y = rect.top;
+	o.w = rect.right-rect.left;
+	o.h = rect.bottom-rect.top;
+	return o;
+    }
 }
